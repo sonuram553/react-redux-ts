54ba7e4f870bf222d55e058e76310686b93d9b4f
Sonu Ram <sonuram6960@gmail.com>
2020-09-06 20:07:24 +0530

    Set up index.ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+
+const store = createStore(reducers, applyMiddleware(thunk))
+
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
